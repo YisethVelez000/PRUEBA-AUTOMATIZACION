b2181a57881477989a64b9c6a74f93bdf97cb5df
YisethVelez000 <yisethvelez07@gmail.com>
2024-12-27 10:51:20 -0500

    Creation contract

--- src/test/java/test/crearContratos.java
+++ src/test/java/test/crearContratos.java
@@ -21,7 +21,7 @@ public class crearContratos
     private long numeroAfiliados;
     @BeforeEach
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\apuertav\\IdeaProjects\\pruebaAutomatizacion\\src\\test\\resources\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\apuertav\\Downloads\\pruebaAutomatizacion\\src\\test\\resources\\chromedriver.exe");
         driver = new ChromeDriver();
         ingreso = new ingresoPruebas(driver);
         configurarConexionDB();
@@ -50,6 +50,13 @@ public class crearContratos
         driver.findElement(By.cssSelector("button#frmContratos\\:j_idt52")).click();
         esperar(1000);
         contratoContributivo();
+        System.out.println("Contrato contributivo creado exitosamente");
+        esperar(10500);
+        driver.findElement(By.cssSelector("button#frmContratos\\:j_idt52")).click();
+        esperar(1000);
+        contratoSubsidiado();
+        System.out.println("Contrato subsidiado creado exitosamente");
+
     }
 
     private void contratoContributivo() throws InterruptedException {
@@ -191,9 +198,127 @@ public class crearContratos
         System.out.println(primeraSede.getText()+ " sede");
         primeraSede.click();
 
-//        driver.findElement(By.cssSelector("input#frmCrearContratoSede\\:nroAfiliados_input")).sendKeys(String.valueOf(numeroAfiliados));
-        driver.findElement(By.id("frmCrearContratoSede:nroAfiliados_input")).sendKeys(String.valueOf(numeroAfiliados));
+        esperar(500);
 
+        driver.findElement(By.id("frmCrearContratoSede:nroAfiliados_input")).sendKeys(String.valueOf(numeroAfiliados));
+        driver.findElement(By.cssSelector("div#frmCrearContratoSede\\:modalidad")).click();
+        // Generamos un numero random para la modalidad de 1 a 5
+        int modalidad = (int) (Math.random() * 5 + 1);
+        System.out.println("Modalidad: " + modalidad);
+        driver.findElement(By.cssSelector("li#frmCrearContratoSede\\:modalidad_" + modalidad)).click();
+
+        int nivelAtencion = (int) (Math.random() * 4 + 1);
+        System.out.println("Nivel de atención: " + nivelAtencion);
+        driver.findElement(By.cssSelector("div#frmCrearContratoSede\\:nivelAtencion")).click();
+        driver.findElement(By.cssSelector("li#frmCrearContratoSede\\:nivelAtencion_" + nivelAtencion)).click();
+
+        // Generamos un número random del 1 al 0, si es 1 = Aplica y si es 0 = No aplica
+        int aplicaContributivo = (int) (Math.random() * 2);
+        System.out.println("Aplica contributivo: " + aplicaContributivo);
+        if (aplicaContributivo == 1) {
+            System.out.println("Aplica contributivo");
+            driver.findElement(By.cssSelector("div#frmCrearContratoSede\\:aplicaContributivo")).click();
+        }else{
+            System.out.println("No aplica contributivo");
+        }
+
+        // Generamos un número random del 1 al 0, si es 1 = Aplica y si es 0 = No aplica
+        int aplicaSubsidiado = (int) (Math.random() * 2);
+        System.out.println("Aplica subsidiado: " + aplicaSubsidiado);
+        if (aplicaSubsidiado == 1) {
+            System.out.println("Aplica subsidiado");
+            driver.findElement(By.cssSelector("div#frmCrearContratoSede\\:aplicaSubsidiado")).click();
+        }else {
+            System.out.println("No aplica subsidiado");
+        }
+
+        // Generamos un número random del 1 al 0, si es 1 = Aplica y si es 0 = No aplica
+        int aplicaGlosa = (int) (Math.random() * 2);
+        System.out.println("Aplica glosa: " + aplicaGlosa);
+        if (aplicaGlosa == 1) {
+            System.out.println("Aplica glosa");
+            driver.findElement(By.cssSelector("div#frmCrearContratoSede\\:aplicaGlosa")).click();
+        }else {
+            System.out.println("No aplica glosa");
+        }
+
+        // Generamos un número random del 1 al 0, si es 1 = Aplica y si es 0 = No aplica
+        int aplicaPAC = (int) (Math.random() * 2);
+        System.out.println("Aplica PAC: " + aplicaPAC);
+        if (aplicaPAC == 1) {
+            System.out.println("Aplica PAC");
+            driver.findElement(By.cssSelector("div#frmCrearContratoSede\\:aplicaPac")).click();
+        }else {
+            System.out.println("No aplica PAC");
+        }
+
+        // Generamos un número random del 1 al 0, si es 1 = Aplica y si es 0 = No aplica
+        int aplicaPortabilidad = (int) (Math.random() * 2);
+        System.out.println("Aplica portabilidad: " + aplicaPortabilidad);
+        if (aplicaPortabilidad == 1) {
+            System.out.println("Aplica portabilidad");
+            driver.findElement(By.cssSelector("div#frmCrearContratoSede\\:aplicaPortabilidad")).click();
+        }else {
+            System.out.println("No aplica portabilidad");
+        }
+
+        // Generamos un número random del 1 al 0, si es 1 = Aplica y si es 0 = No aplica
+        int aplicaAuditoria = (int) (Math.random() * 2);
+        System.out.println("Aplica auditoria: " + aplicaAuditoria);
+        if (aplicaAuditoria == 1) {
+            System.out.println("Aplica auditoria");
+            driver.findElement(By.cssSelector("div#frmCrearContratoSede\\:aplicaAuditoria")).click();
+        }else {
+            System.out.println("No aplica auditoria");
+        }
+
+        // Generamos un número random del 1 al 0, si es 1 = Aplica y si es 0 = No aplica
+        int aplicaAutorizacion = (int) (Math.random() * 2);
+        System.out.println("Aplica autorizacion: " + aplicaAutorizacion);
+        if (aplicaAutorizacion == 1) {
+            System.out.println("Aplica autorizacion");
+            driver.findElement(By.cssSelector("div#frmCrearContratoSede\\:aplicaAutorización")).click();
+        }else {
+            System.out.println("No aplica autorizacion");
+        }
+
+        // Generamos un número random del 1 al 0, si es 1 = Aplica y si es 0 = No aplica
+        int aplicaAgendamiento = (int) (Math.random() * 2);
+        System.out.println("Aplica agendamiento: " + aplicaAgendamiento);
+        if (aplicaAgendamiento == 1) {
+            System.out.println("Aplica agendamiento");
+            driver.findElement(By.cssSelector("div#frmCrearContratoSede\\:aplicaAgendamiento")).click();
+        }else {
+            System.out.println("No aplica agendamiento");
+        }
+
+        esperar(200);
+        driver.findElement(By.id("frmCrearContratoSede:valorContrato_input")).sendKeys(String.valueOf(valorContrato));
+
+        // Generamos un número random del 1 al 0, si es 1 = Aplica y si es 0 = No aplica
+        int aplicaRecaudoCopago = (int) (Math.random() * 2);
+        System.out.println("Aplica recaudo copago: " + aplicaRecaudoCopago);
+        if (aplicaRecaudoCopago == 1) {
+            System.out.println("Aplica recaudo copago");
+            driver.findElement(By.cssSelector("div#frmCrearContratoSede\\:aplicaCopago")).click();
+        }else {
+            System.out.println("No aplica recaudo copago");
+        }
+
+        // Traemos la fecha actual
+        LocalDate fechaInicio = LocalDate.now();
+        driver.findElement(By.cssSelector("input#frmCrearContratoSede\\:fechaInicio_input")).sendKeys(String.valueOf(fechaInicio));
+
+        // Le sumamos 6 meses a la fecha actual
+        LocalDate fechaFinal = LocalDate.now();
+        fechaFinal = fechaFinal.plusMonths(6);
+        driver.findElement(By.cssSelector("input#frmCrearContratoSede\\:fechaFin_input")).sendKeys(String.valueOf(fechaFinal));
+
+        driver.findElement(By.cssSelector("textarea#frmCrearContratoSede\\:observacion")).sendKeys("Prestar servicios integrales de salud de baja complejidad a los afiliados del Régimen subsidiado, contributivo y en estado de portabilidad y movilidad de Savia Salud EPS, del Municipio de Angostura y que se encuentren debidamente registrados en la base de datos que dispone la EPS y con derecho a los servicios contenidos en el Plan de Beneficios de Salud y la atención en todos los servicios contratados como Prestador Primario de Salud.");
+
+        driver.findElement(By.cssSelector("button#frmCrearContratoSede\\:j_idt441")).click();
+
+        driver.findElement(By.cssSelector("button#frmCrear\\:j_idt242")).click();
     }
 
 }
